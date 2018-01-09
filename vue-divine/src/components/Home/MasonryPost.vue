8a1cd14715c022b68bc2c12778d0d3c55672e853
Karl <starwolf@sbcglobal.net>
2018-01-09 14:28:55 -0800

    Dynamically stored posts in object array.

--- vue-divine/src/components/Home/MasonryPost.vue
+++ vue-divine/src/components/Home/MasonryPost.vue
@@ -1,11 +1,19 @@
 <template>
     <div class="singlePost">
-        <h2>Single Post</h2>
-        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.</p>
+        <h2>{{ data.title }}</h2>
+        <p>{{ data.message }}</p>
     </div>
 </template>
 
 <script>
+    export default {
+        props: {
+            data: {
+                type: Object,
+                required: true
+            }
+        }
+    }
 
 </script>
 
--- vue-divine/src/components/Home/MasonryPosts.vue
+++ vue-divine/src/components/Home/MasonryPosts.vue
@@ -1,14 +1,7 @@
 <template>
     <div id="masonryPosts">
-        <app-masonryPost class="item"></app-masonryPost>
-        <app-masonryPost class="item"></app-masonryPost>
-        <app-masonryPost class="item"></app-masonryPost>
-        <app-masonryPost class="item"></app-masonryPost>
-        <app-masonryPost class="item"></app-masonryPost>
-        <app-masonryPost class="item"></app-masonryPost>
-        <app-masonryPost class="item"></app-masonryPost>
-        <app-masonryPost class="item"></app-masonryPost>
-        <app-masonryPost class="item"></app-masonryPost>
+        <!--<app-masonryPost class="item"></app-masonryPost>-->
+        <app-masonryPost class="item" v-for="post in posts" :data="post"></app-masonryPost>
     </div>
 </template>
 
@@ -18,13 +11,25 @@ import MasonryPost from './MasonryPost';
 export default{
     components: {
         appMasonryPost: MasonryPost
+    },
+    data: function () {
+        return {
+            posts: [
+                {title: 'MyTitle', author: 'My Name', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+                {title: 'MyTitle2', author: 'My Name2', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+                {title: 'MyTitle3', author: 'My Name3', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+                {title: 'MyTitle4', author: 'My Name', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+                {title: 'MyTitle5', author: 'My Name2', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'},
+                {title: 'MyTitle6', author: 'My Name3', message: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.'}
+            ]
+        }
     }
 }
 
 </script>
 
 <style scoped>
-    #masonryPosts{
+    #masonryPosts {
         column-count: 3;
         column-gap: 2em;
     }
