5a859a247b7a430425d2371727726efdaa4ef05f
Karl <starwolf@sbcglobal.net>
2018-04-03 20:38:39 -0700

    Small Icon modification + git commit test.

--- vue-divine/src/components/Home/MasonryPost.vue
+++ vue-divine/src/components/Home/MasonryPost.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="singlePost">
         <h2>Title: {{ data.title }}</h2>
-        <button @click="deletePost" class="delete">x</button>
+        <button @click="deletePost" class="delete"><icon name="close" scale="1.5"></icon></button>
         <p>Index: {{ index }}</p
         <p>Message: {{ data.message }}</p>
         <p>{{ data.timeStamp }}</p>
@@ -28,6 +28,10 @@
 </script>
 
 <style scoped>
+    button{
+        border: none;
+        background: none;
+    }
     .singlePost{
         padding: 25px;
         border: 2px solid black;
@@ -36,15 +40,14 @@
         background: #febdff;
     }
     .delete{
-        border: 2px solid #d0d0d0;
-        padding: 5px 10px;
         position: relative;
         left: 92%;
-        bottom: 27px;
+        bottom: 35px;
     }
     .delete:hover{
         background: #72ffb1;
         color: white;
         cursor:pointer;
+        border-radius: 50%;
     }
 </style>
