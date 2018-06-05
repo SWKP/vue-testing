8f9b0383ec54d61b2c143f00dacf16af6bcf1de3
connor <connormcn37@gmail.com>
2018-06-05 16:59:13 -0700

    wisdom

--- vue-divine/src/components/Home/HomeEventFeed.vue
+++ vue-divine/src/components/Home/HomeEventFeed.vue
@@ -5,6 +5,7 @@
             <div class="singleItem"><icon name="quote-left" scale="1"></icon>The best preparation for tomorrow is doing your best today.<icon name="quote-right" scale="1"></icon></div>
             <div class="singleItem"><icon name="quote-left" scale="1"></icon>Always pass on what you have learned.<icon name="quote-right" scale="1"></icon></div>
             <div class="singleItem"><icon name="quote-left" scale="1"></icon>Death is a natural part of life. Rejoice for those around you who transform into the Force. Mourn them do not. Miss them do not. Attachment leads to jealously. The shadow of greed, that is.<icon name="quote-right" scale="1"></icon></div>
+            <div class="singleItem"><icon name="quote-left" scale="1"></icon>Did you ever hear the tragedy of Darth Plagueis The Wise? I thought not. It's not a story the Jedi would tell you. It's a Sith legend. Darth Plagueis was a Dark Lord of the Sith, so powerful and so wise he could use the Force to influence the midichlorians to create life… He had such a knowledge of the dark side that he could even keep the ones he cared about from dying. The dark side of the Force is a pathway to many abilities some consider to be unnatural. He became so powerful… the only thing he was afraid of was losing his power, which eventually, of course, he did. Unfortunately, he taught his apprentice everything he knew, then his apprentice killed him in his sleep. Ironic. He could save others from death, but not himself.<icon name="quote-right" scale="1"></icon></div>  
         </div>
     </div>
 </template>
@@ -74,7 +75,7 @@
               animation-duration: 80s;
     }
     .singleItem{
-        
+
         color:#fff;
         display: inline-block;
         padding: .25rem 2rem 0;
